40e36e8eb5191cbb4dd3da74d202e8f4f0cc7570
grz.szu <grz.szu@9769f836-e5e6-11de-822f-6f1ea0f87eb3>
2009-12-17 22:11:26 +0000

    added hash functions universal and zobrist.

---
+++ trunk/hash_functions/universal/hash_function.h
@@ -0,0 +1,30 @@
+#ifndef HASH_FUNCTION_H
+#define HASH_FUNCTION_H
+
+typedef int hash_data[32];
+
+void hash_init(hash_data a) {
+  int i;
+  for(i=0; i<32; i++) {
+    a[i] = (int)hash_rand();
+  }
+}
+
+int hash(hash_data a, int key) {
+  int h, i;
+  for (i=0; i<(4<<3); i+=8)
+  {
+    char k = ((char*)key)[i>>3];
+    if(k&0x01) h ^= a[i+0];
+    if(k&0x02) h ^= a[i+1];
+    if(k&0x04) h ^= a[i+2];
+    if(k&0x08) h ^= a[i+3];
+    if(k&0x10) h ^= a[i+4];
+    if(k&0x20) h ^= a[i+5];
+    if(k&0x40) h ^= a[i+6];
+    if(k&0x80) h ^= a[i+7];
+  }
+  return hash;
+}
+
+#endif
---
+++ trunk/hash_functions/zobrist/hash_function.h
@@ -0,0 +1,20 @@
+#ifndef HASH_FUNCTION_H
+#define HASH_FUNCTION_H
+
+typedef int hash_data[4][256];
+
+void hash_init(hash_data a) {
+  int i;
+  for(i=0; i<32; i++) {
+    a[i] = (int)hash_rand();
+  }
+}
+
+int hash(hash_data a, int key, int mask) {
+  int h, i;
+  for(i=0; i<4; ++i)
+    h ^= a[i][((char*)key)[i]];
+  return h & mask;
+}
+
+#endif
