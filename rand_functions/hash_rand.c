a0a69992f2dee81704b00c2c43edfacb9182a985
grz.szu <grz.szu@9769f836-e5e6-11de-822f-6f1ea0f87eb3>
2009-12-17 22:48:14 +0000

    added hash_rand.c and hash_rand.h to read random bits from a file.

---
+++ rand_functions/hash_rand.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static FILE rand_f;
+
+void hash_rand_init(char *fn) {
+  FILE *rand_f = fopen(fn, "r");
+  if(rand_f) {
+    //buffer ?
+  } else {
+    fprintf(stderr, "error: failed to open random data file [%s]\n", fn);
+    exit(1);
+  }
+}
+
+int hash_rand() {
+  int r;
+  if(fread(&r, sizeof(int), 1, rand_f) != 1) {
+    if(ferror(rand_f)) {
+
+    } else if (feof(rand_f)) {
+
+    }
+  } else {
+    return r;
+  }
+}
