970109c8d2b4ca4cb54170c03c457ee130156422
grz.szu <grz.szu@9769f836-e5e6-11de-822f-6f1ea0f87eb3>
2009-12-17 23:56:27 +0000

    updated hash_rand.c with buffering.

--- rand_functions/hash_rand.c
+++ rand_functions/hash_rand.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 static FILE *rand_f;
+static int *rand_buf;
+static int ri;
+static int rand_size;
 
 void hash_rand_init(char *fn) {
+  struct stat st;
   rand_f = fopen(fn, "r");
   if(rand_f) {
-    //buffer ?
+    fstat(fileno(rand_f), &st);
+    rand_size = st.st_size;
+    for(ri=0; ri<rand_size/4; ri++) {
+      if(fread(rand_buf+ri, sizeof(int), 1, rand_f) != 1) {
+        if(ferror(rand_f)) {
+          fprintf(stderr, "error: failed reading file [%s]\n", fn);
+          exit(1);
+        }
+/*
+        else if (feof(rand_f)) {
+          
+        }
+*/
+      }
+    }
+    ri=0;
   } else {
     fprintf(stderr, "error: failed to open random data file [%s]\n", fn);
     exit(1);
@@ -14,14 +35,5 @@ void hash_rand_init(char *fn) {
 }
 
 int hash_rand() {
-  int r;
-  if(fread(&r, sizeof(int), 1, rand_f) != 1) {
-    if(ferror(rand_f)) {
-
-    } else if (feof(rand_f)) {
-
-    }
-  } else {
-    return r;
-  }
+  return rand_buf[ri++];
 }
