89a77e9f58fe7643b1e3a21e2e6ebabe7e032953
grz.szu <grz.szu@9769f836-e5e6-11de-822f-6f1ea0f87eb3>
2009-12-18 19:18:54 +0000

    added rotating hash function

---
+++ hash_functions/rotating/hash_function.c
@@ -0,0 +1,11 @@
+typedef int hash_data;
+
+void hash_init(hash_data a) {
+}
+
+int hash(hash_data a, int key, int mask) {
+  int h=0, i;
+  for(i=0; i<4; ++i)
+    h = (h<<4)^(h>>28)^((char*)&key)[i];
+  return h & mask;
+}
