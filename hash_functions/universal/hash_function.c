d3de72b5e858e9a109ec97f0cd0bb75aa194468a
grz.szu <grz.szu@9769f836-e5e6-11de-822f-6f1ea0f87eb3>
2009-12-18 18:44:57 +0000

    fixed universal and zobrist

--- hash_functions/universal/hash_function.c
+++ hash_functions/universal/hash_function.c
@@ -11,7 +11,7 @@ int hash(hash_data a, int key, int mask) {
   int h, i;
   for (i=0; i<(4<<3); i+=8)
   {
-    char k = ((char*)key)[i>>3];
+    char k = ((char*)&key)[i>>3];
     if(k&0x01) h ^= a[i+0];
     if(k&0x02) h ^= a[i+1];
     if(k&0x04) h ^= a[i+2];
